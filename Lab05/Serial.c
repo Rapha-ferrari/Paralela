1ff2822d4b74d006ac665a97cda23088d089a958
Raphael Ferrari <64502502+Rapha-ferrari@users.noreply.github.com>
2023-10-23 17:43:14 -0300

    Serial.c

---
+++ Lab05/Serial.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <math.h>
+
+double f(double x) {
+    return exp(x);
+}
+
+double trapezoidal(double a, double b, int n) {
+    double h = (b - a) / n;
+    double sum = 0.5 * (f(a) + f(b));
+
+    for (int i = 1; i < n; i++) {
+        double x = a + i * h;
+        sum += f(x);
+    }
+
+    return h * sum;
+}
+
+int main() {
+    double a, b;
+    int n;
+
+    printf("Digite o limite inferior (a): ");
+    scanf("%lf", &a);
+    printf("Digite o limite superior (b): ");
+    scanf("%lf", &b);
+    printf("Digite o número de subintervalos (n): ");
+    scanf("%d", &n);
+
+    if (n <= 0) {
+        printf("O número de subintervalos deve ser positivo.\n");
+    } else {
+        double result = trapezoidal(a, b, n);
+        printf("Aproximação da integral: %lf\n", result);
+    }
+
+    return 0;
+}
